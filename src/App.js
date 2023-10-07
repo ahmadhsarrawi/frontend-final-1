77a4f3b552d5119fcb28f40f355404481e7a7e74
Shahd Hassoun <shahdhassoun2000@gmail.com>
2023-10-07 03:50:47 +0300

    setting main files for header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import ContextProvider from "./store/context";
-
+import Header from "./components/Header"
 
 
 
@@ -11,6 +11,7 @@ function App() {
   return (
     <ContextProvider>
       <BrowserRouter basename="frontend-final-1">
+        <Header/>
         <Routes>
           <Route path="/" element={<Landing />} />
         </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ReactComponent as Logo } from '../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Header>
+    <Logo/>
+    <div>Header</div>
+    </Header>
+    
+  )
+
+}
